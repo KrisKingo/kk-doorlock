73eea859592e03a9077eb69215dd89a9696d4f7d
KrisKingo <50709418+KrisKingo@users.noreply.github.com>
2021-09-29 21:50:24 +0200

    New auto lock doors

--- kk-doorlock/server.lua
+++ kk-doorlock/server.lua
@@ -1,13 +1,17 @@
 RegisterServerEvent('kk-doorlock:server:updateState')
-AddEventHandler('kk-doorlock:server:updateState', function(door)
+AddEventHandler('kk-doorlock:server:updateState', function(door, forced)
     local doorKey = tonumber(door)
 
     if not doorKey or not Config.Doors[doorKey] then
         return
     end
 
+    if forced == nil then
+        forced = false
+    end
+
     Config.Doors[doorKey].locked = not Config.Doors[doorKey].locked
-    TriggerClientEvent('kk-doorlock:client:updateState', -1, doorKey, Config.Doors[doorKey].locked)
+    TriggerClientEvent('kk-doorlock:client:updateState', -1, doorKey, Config.Doors[doorKey].locked, forced)
 end)
 
 
